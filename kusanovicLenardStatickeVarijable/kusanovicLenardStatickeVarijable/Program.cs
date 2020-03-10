2262b5a5e8913accfff149b17913cfbf03ae7bfb
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2020-03-10 12:22:44 +0100

    Add files via upload

---
+++ kusanovicLenardStatickeVarijable/kusanovicLenardStatickeVarijable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardStatickeVarijable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa prvi = new PrvaKlasa("Pero", 2003);
+            Console.WriteLine(prvi.ToString());
+
+            PrvaKlasa drugi = new PrvaKlasa("Ivica", 2004);
+            Console.WriteLine(drugi.ToString());
+
+            PrvaKlasa treci = new PrvaKlasa("Ibro", 2001);
+            Console.WriteLine(treci.ToString());
+
+            treci.Ime = "Milorad";
+
+            Console.WriteLine(treci.Ime);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ kusanovicLenardStatickeVarijable/kusanovicLenardStatickeVarijable/PrvaKlasa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardStatickeVarijable
+{
+    class PrvaKlasa
+    {
+        string ime;
+        int godina;
+
+        static int brojac = 0;
+
+        public override string ToString()
+        {
+            string ispis = "\n Ime: " + ime + "\n Godiste:" + godina + "g" + "\n RB: " + brojac;
+            return ispis;
+        }
+
+        public string Ime { get => ime; set => ime = value; }
+        public int Godina { get => godina; set => godina = value; }
+
+        public PrvaKlasa(string ime, int godina)
+        {
+            brojac++;
+            this.ime = ime;
+            this.godina = godina;
+        }
+    }
+}
