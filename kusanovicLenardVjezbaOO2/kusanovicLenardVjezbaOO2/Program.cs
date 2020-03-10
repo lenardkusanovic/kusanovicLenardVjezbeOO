9b62daa61e924de808b52aa1f3397f7a16ced542
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2020-03-10 12:24:43 +0100

    Add files via upload

---
+++ kusanovicLenardVjezbaOO2/kusanovicLenardVjezbaOO2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardVjezbaOO2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vozilo auto = new Vozilo("Auto", 4);
+            Console.WriteLine("Upisite maximalnu brzinu automobila: ");
+            auto.MaxBrzina = Console.ReadLine(Convert.ToInt32());
+        }
+    }
+}
---
+++ kusanovicLenardVjezbaOO2/kusanovicLenardVjezbaOO2/Vozilo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardVjezbaOO2
+{
+    class Vozilo
+    {
+        string vrsta;
+        int maxBrzina;
+        int brojKotaca;
+
+        public override string ToString()
+        {
+            string ispis = "\nVrsta automobila: " + vrsta + "\n Max brzina: " + maxBrzina + "\n Broj kotaca:" + brojKotaca; 
+            return ispis;
+        }
+
+
+        public string Vrsta { get => vrsta; set => vrsta = value; }
+        public int MaxBrzina { get => maxBrzina; set => maxBrzina = value; }
+        public int BrojKotaca { get => brojKotaca; set => brojKotaca = value; }
+
+        public Vozilo(string vrsta, int maxBrzina, int brojKotaca)
+        {
+            this.vrsta = vrsta;
+            this.maxBrzina = maxBrzina;
+            this.brojKotaca = brojKotaca;
+        }
+    }
+}
