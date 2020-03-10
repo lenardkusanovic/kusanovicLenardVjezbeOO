41573b3abd6803b301a0b4783b64c0997faf6ce7
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2020-03-10 11:45:17 +0100

    Add files via upload

---
+++ kusanovicLenardVjezbaOO/kusanovicLenardVjezbaOO/Neboder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardVjezbaOO
+{
+    class Neboder
+    {
+        float visina;
+        int brojKatova;
+
+        public override string ToString(float prosjekKata)
+        {
+            string ispis = "\n Visina nebodera: " + visina + "m" + "\n Broj katova: " + brojKatova + "\n Prosjecna visina kata: " + prosjekKata "m";
+            return ispis;
+        }
+
+        public float getVisina()
+        {
+            return visina;
+
+        }
+
+        public int getBrojKatova()
+        {
+            return brojKatova;
+        }
+
+        public void setVisina(float visina)
+        {
+            this.visina = visina;
+        }
+
+        public void setBrojKatova(int brojKatova)
+        {
+            this.brojKatova = brojKatova;
+        }
+
+        public Neboder(float visina, int brojKatova)
+        {
+            this.visina = visina;
+            this.brojKatova = brojKatova;
+        }
+    }
+}
---
+++ kusanovicLenardVjezbaOO/kusanovicLenardVjezbaOO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardVjezbaOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float prosjekKata;
+
+            Neboder Tower = new Neboder(200,100);
+
+            Tower.setVisina(Tower.getVisina() + 10);
+            Tower.setBrojKatova(Tower.getBrojKatova() + 1);
+
+            prosjekKata = Tower.getVisina() / Tower.getBrojKatova();
+
+            
+            
+            Console.WriteLine(Tower.ToString() + "\n Prosjecna visina kata: " + prosjekKata "m";);
+            Console.ReadKey();
+
+
+        }
+    }
+}
